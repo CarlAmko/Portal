d1e85fd4137c51b939bf04e9d7078da9394b8e6a
Carl <carl@carlamko.me>
2019-01-06 14:52:13 -0500

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,43 @@
+package me.carlamko
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.features.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    // http://ktor.io/servers/features/https-redirect.html#testing
+    if (!testing) {
+        install(HttpsRedirect) {
+            // The port to redirect to. By default 443, the default HTTPS port.
+            sslPort = 443
+            // 301 Moved Permanently, or 302 Found redirect.
+            permanentRedirect = true
+        }
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
